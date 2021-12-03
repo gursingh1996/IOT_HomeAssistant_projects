d31192efaab26fa6833c9607683dbfc8e3e894a9
Gurpreet <gurpreetsingh.panesar1996@gmail.com>
2021-12-03 12:17:09 +0530

    Added alexa control and OTA programming

--- Gurpreet-home/Gurpreet_BedRoom/Gurpreet_BedRoom.ino
+++ Gurpreet-home/Gurpreet_BedRoom/Gurpreet_BedRoom.ino
@@ -1,8 +1,9 @@
 #include <ESP8266WiFi.h>
-#include <PubSubClient.h>
-
-// Update these with values suitable for your network.
-#define status_led BUILTIN_LED
+#include <PubSubClient.h>   //required for mqtt
+#include <Espalexa.h>    //required for alexa
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 #define SH_CP 16
 #define ST_CP 14
@@ -24,13 +25,20 @@ const char* ssid = "Tenda_E0B350";
 const char* password = "meetupper430";
 const char* mqtt_server = "192.168.0.108";
 
+void lightChanged(EspalexaDevice* dev);   //callback functions //callback type, contains device pointer
+void fanChanged(EspalexaDevice* dev);   //callback functions //callback type, contains device pointer
+
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+Espalexa espalexa;    //esp alexa device
+
+EspalexaDevice* Light;
+EspalexaDevice* Fan;
 
 long lastMsg = 0;
 char msg[50];
 int value = 0;
-
 
 void setup_wifi() {
   delay(10);
@@ -38,7 +46,8 @@ void setup_wifi() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
+  WiFi.hostname("ESP_Bedroom");
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -118,7 +127,49 @@ void setup() {
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-  pinMode(BUILTIN_LED, OUTPUT);
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Light = new EspalexaDevice("Light", lightChanged, EspalexaDeviceType::onoff); //ON - OFF light
+  espalexa.addDevice(Light);
+  Fan = new EspalexaDevice("Fan", fanChanged, EspalexaDeviceType::onoff); //ON - OFF fan
+  espalexa.addDevice(Fan);
+  
+  espalexa.begin();
   pinMode(EN, OUTPUT);
   digitalWrite(EN, LOW);      //donot read the noise - 74hc595
   pinMode(SH_CP, OUTPUT);
@@ -130,9 +181,44 @@ void setup() {
   shiftOut(dataPin, clockPin, LSBFIRST, data);
   digitalWrite(latchPin, HIGH);
 }
+
 void loop() {
+  ArduinoOTA.handle();
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
+  if(WiFi.status() != WL_CONNECTED) setup_wifi();   //if wifi disconnects try reconnecting
+  espalexa.loop();        //look for alexa commands
+  delay(1);
+}
+
+void lightChanged(EspalexaDevice* d) {    //call back function
+  if (d == nullptr) return; //this is good practice, but not required
+  Serial.print("Light changed to ");
+  if (d->getValue()){
+    Serial.println("Light is now ON");
+    writeData(lightPin, 1);
+    client.publish("/gurpreetRoom/light/state", "a");
+  }
+  else {
+    Serial.println("OFF");
+    writeData(lightPin, 0);
+    client.publish("/gurpreetRoom/light/state", "b");
+  }
+}
+
+void fanChanged(EspalexaDevice* d) {    //call back function
+  if (d == nullptr) return; //this is good practice, but not required
+  Serial.print("fan changed to ");
+  if (d->getValue()){
+    Serial.println("Fan is now ON");
+    writeData(fanPin, 1);
+    client.publish("/gurpreetRoom/fan/state", "c"); 
+  }
+  else {
+    Serial.println("OFF");
+    writeData(fanPin, 0);
+    client.publish("/gurpreetRoom/fan/state", "d"); 
+  }
 }
