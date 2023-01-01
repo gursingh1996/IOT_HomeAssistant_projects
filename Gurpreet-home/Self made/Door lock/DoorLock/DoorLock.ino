ca2fd4a6f2afe1c68b6ce6f238deb1fbc96462d6
Gurpreet <gurpreetsingh.panesar1996@gmail.com>
2023-01-01 20:31:45 +0530

    New code for door lock without home assistant

---
+++ Gurpreet-home/Self made/Door lock/DoorLock/DoorLock.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(1000);
+  Serial.print("Started");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
